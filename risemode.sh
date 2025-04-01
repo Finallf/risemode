097b9bf9d6cb7648e99b96f73b11b8b439e4377f
Regis Vieira Delgado <Finallf@users.noreply.github.com>
2025-04-01 00:07:46 -0300

    Atualizado checagem USB

--- risemode.sh
+++ risemode.sh
@@ -1,5 +1,19 @@
 #!/bin/bash
 
+VENDOR_ID=AA88	# Rise Mode Aura Ice 0xAA88
+PRODUCT_ID=8666	# Rise Mode Aura Ice 0x8666
+FILES=/dev/hidraw*
+
+for f in $FILES; do
+	FILE=${f##*/}
+	VID="$(cat /sys/class/hidraw/${FILE}/device/uevent | grep -oP 'HID_ID.{10}\K.{4}')"
+	PID="$(cat /sys/class/hidraw/${FILE}/device/uevent | grep -oP 'HID_ID.{19}\K.{4}')"
+	if [ $VID == $VENDOR_ID ] && [ $PID == $PRODUCT_ID ]; then
+		HIDRAW=$FILE
+		break
+	fi
+done
+
 function get_temp {
 	TEMP="$(sensors 2>/dev/null | grep -oP 'CPU.*?\+\K[0-9]+')"
 	TEMP="$(printf '%x\n' $TEMP)"
@@ -7,6 +21,6 @@ function get_temp {
 
 while :; do
 	get_temp
-	echo -e \\x${TEMP} > /dev/INSTALL
+	echo -e \\x${TEMP} > /dev/${HIDRAW}
 	sleep 2.1
 done
